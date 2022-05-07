e9cfd23b06289fbdef42e1f7daf5f400e4c2c0b3
e1k1to <eikimillian@protonmail.com>
2022-05-07 00:28:57 -0300

    Adicionada bind para screenshot

--- config.h
+++ config.h
@@ -61,20 +61,21 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *firefox[]  = { "firefox"  , NULL };
-static const char *firefoxpr[]= { "firefox", "-private", NULL };
+static const char *firefoxpr[]= { "firefox"  , "-private", NULL };
+static const char *discord[]  = { "discord"  , "NULL" };
 static const char *incrlight[]= { "brightnessctl", "set", "+5%", NULL};
 static const char *decrlight[]= { "brightnessctl", "set", "5%-", NULL};
-
+static const char *screenshot[]= { "screenshot", NULL };
 static Key keys[] = {
 	/* modifier                     key  	  function       	 argument */
 	{ MODKEY,                       XK_p,      spawn,         	{.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,         	{.v = firefox  } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,         	{.v = firefoxpr} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,         	{.v = termcmd  } },
-	{0,				XF86XK_MonBrightnessUp,
-						   spawn,		{.v = incrlight} },
-	{0,				XF86XK_MonBrightnessDown,
-						   spawn,		{.v = decrlight} },
+	{0,								XF86XK_MonBrightnessUp,    spawn,		{.v = incrlight} },
+	{0,								XF86XK_MonBrightnessDown,  spawn,		{.v = decrlight} },
+	{0,								XK_Print,  spawn,			{.v = screenshot } },
+	{ MODKEY|ShiftMask,				XK_d,	   spawn,			{.v = discord  } },
 	{ MODKEY,                       XK_b,      togglebar,     	{0} },
 	{ MODKEY,                       XK_j,      focusstack,    	{.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,    	{.i = -1 } },
