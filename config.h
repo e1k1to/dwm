43742a8f6c1616f26c3d18378e9d2f65bedb162e
e1k1to <eikimillian@protonmail.com>
2022-06-02 02:22:58 -0300

    sla

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "ttf-liberation:size=14","Font Awesome 5 Pro:size=14" };
+static const char dmenufont[]       = "ttf-liberation:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
